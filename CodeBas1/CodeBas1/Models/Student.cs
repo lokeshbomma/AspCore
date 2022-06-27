12f5f9389408c0f3dba6e937d17fd600cee92a6f
Lokesh Bomma <l.bomma@zensar.com>
2022-06-27 20:57:39 +0530

    WebApplication2

---
+++ CodeBas1/CodeBas1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeBas1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Branch { get; set; }
+        public string Location { get; set; }
+    }
+}
